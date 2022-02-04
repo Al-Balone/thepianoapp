5e47555786aae823c8b0779e867bae7848adee7d
Al-Balone <85862719+Al-Balone@users.noreply.github.com>
2022-02-04 11:48:25 +0000

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Piano from './components/Front/piano/piano';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Piano />
   </React.StrictMode>,
   document.getElementById('root')
 );
